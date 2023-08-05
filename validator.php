549ffc2549f1a71a4a4391d891ea78703b1d038c
yPolar <111068845+yPolar0@users.noreply.github.com>
2023-08-04 22:33:49 -0300

    Update validator.php

--- validator.php
+++ validator.php
@@ -1,14 +1,17 @@
+<?php
+header('Content-Type: application/json');
+
 function isValidKey($key) {
     $secretKey = '2kQn5nBp7GdFm0jvXZOIzYLCelWwRJhUH43iPE0T';
     return $key === $secretKey;
 }
 
 $key = $_GET['key'] ?? '';
-
 $response = ['status' => 'Invalid'];
-if (isValidKey($key)) {
+
+if (strlen($key) === 40 && ctype_alnum($key) && isValidKey($key)) {
     $response['status'] = 'Active';
 }
 
-header('Content-Type: application/json');
 echo json_encode($response);
+?>
