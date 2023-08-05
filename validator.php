8c7992d9aae1804d6ab548e1373556baa6f2bc19
yPolar <111068845+yPolar0@users.noreply.github.com>
2023-08-04 23:32:07 -0300

    Delete validator.php

--- validator.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$keys = array("2kQn5nBp7GdFm0jvXZOIzYLCelWwRJhUH43iPE0T"); //Add Key here Example: ("test", "asd", "asdasdasd", "gasdasd")
-$testing = $_GET["key"];
-if (in_array($testing,$keys,TRUE)) {
-  echo "true"; 
-} else {
-  echo "false"; 
-}
-?>
